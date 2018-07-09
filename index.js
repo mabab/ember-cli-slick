c23e0ccdde2765a9542419228ec64a03032e84a1
Max Babich <max22dev@gmail.com>
2018-07-09 16:03:11 +0300

    upgrade ember version and use only node assets

--- index.js
+++ index.js
@@ -21,14 +21,17 @@ module.exports = {
 
 
   included: function(app) {
+
+
+
     this._super.included(app);
 
-    app.import(app.nodeAssets + '/slick-carousel/slick/slick.css');
-    app.import(app.nodeAssets + '/slick-carousel/slick/slick-theme.css');
-    app.import(app.nodeAssets + '/slick-carousel/slick/fonts/slick.ttf', { destDir: 'assets/fonts' });
-    app.import(app.nodeAssets + '/slick-carousel/slick/fonts/slick.svg', { destDir: 'assets/fonts' });
-    app.import(app.nodeAssets + '/slick-carousel/slick/fonts/slick.eot', { destDir: 'assets/fonts' });
-    app.import(app.nodeAssets + '/slick-carousel/slick/fonts/slick.woff', { destDir: 'assets/fonts' });
-    app.import(app.nodeAssets + '/slick-carousel/slick/ajax-loader.gif', { destDir: 'assets' });
+    app.import('vendor/slick-carousel/slick/slick.css');
+    app.import('vendor/slick-carousel/slick/slick-theme.css');
+    app.import('vendor/slick-carousel/slick/fonts/slick.ttf', { destDir: 'assets/fonts' });
+    app.import('vendor/slick-carousel/slick/fonts/slick.svg', { destDir: 'assets/fonts' });
+    app.import('vendor/slick-carousel/slick/fonts/slick.eot', { destDir: 'assets/fonts' });
+    app.import('vendor/slick-carousel/slick/fonts/slick.woff', { destDir: 'assets/fonts' });
+    app.import('vendor/slick-carousel/slick/ajax-loader.gif', { destDir: 'assets' });
   }
 };
