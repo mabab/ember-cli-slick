02d3a123f7226fdba2ec1370841d1be6d384ee25
Max Babich <max22dev@gmail.com>
2018-08-28 12:06:04 +0300

    upgrade ember version and use only node assets

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const fastbootTransform = require('fastboot-transform');
 
 module.exports = {
     name: 'ember-cli-slick',
-
-    nodeAssets: {
-        'slick-carousel': {
-            vendor: {
-                include: ['slick/slick.js', 'slick/slick.css', 'slick/slick-theme.css'],
-                processTree(input) {
-                    return fastbootTransform(input);
+    options: {
+        nodeAssets: {
+            'slick-carousel': {
+                vendor: {
+                    include: ['slick/slick.js', 'slick/slick.css', 'slick/slick-theme.css'],
+                    processTree(input) {
+                        return fastbootTransform(input);
+                    }
+                },
+                public: {
+                    include: ['slick/ajax-loader.gif', 'slick/fonts/*']
                 }
-            },
-            public: {
-                include: ['slick/ajax-loader.gif', 'slick/fonts/*']
             }
         }
     }
